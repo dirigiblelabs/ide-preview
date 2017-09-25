8644f606b7c142a00ce358209af5b1298a09ac39
delchev <delchevn@gmail.com>
2017-09-25 23:53:03 +0300

    view description enhanced

---
+++ ide-preview/preview-view.js
@@ -0,0 +1,11 @@
+exports.getView = function() {
+	var view = {
+			"id":"preview",
+			"name":"Preview",
+			"factory":"frame",
+			"region":"center-bottom",
+			"label":"Preview",
+			"link":"../ide-preview/preview.html"
+	};
+	return view;
+}
